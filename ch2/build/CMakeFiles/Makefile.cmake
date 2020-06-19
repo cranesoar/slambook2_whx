789984f246d4e27a233eae5e71173625b981c497
cranesoar <141365535@qq.com>
2020-06-20 01:04:50 +0800

    迷之cmake

--- ch2/build/CMakeFiles/Makefile.cmake
+++ ch2/build/CMakeFiles/Makefile.cmake
@@ -42,8 +42,4 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/hello_shared.dir/DependInfo.cmake"
-  "CMakeFiles/hello.dir/DependInfo.cmake"
-  "CMakeFiles/helloSLAM.dir/DependInfo.cmake"
-  "CMakeFiles/useHello.dir/DependInfo.cmake"
   )
