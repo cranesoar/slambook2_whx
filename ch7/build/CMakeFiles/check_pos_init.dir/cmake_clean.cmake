b4bdf3cdf07a35efe725151bbdbefc42f85775a9
cranesoar <141365535@qq.com>
2021-05-08 11:04:48 +0800

    添加第七课

---
+++ ch7/build/CMakeFiles/check_pos_init.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check_pos_init.dir/check_pos_init.cpp.o"
+  "check_pos_init.pdb"
+  "check_pos_init"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check_pos_init.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
